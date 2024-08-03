f3e4e0e8ac01a84a700113a13ec344e4ee9cf011
Emmanuel Nnaji <realreaach@yahoo.com>
2024-08-03 05:30:26 +0100

    fxn returns set from an array param

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+const setFromArray = (array) => {
+  // Check if the argument is an array
+  if (!Array.isArray(array)) {
+    throw new Error('The argument must be an array');
+  }
+
+  // Create and return a Set from the array
+  return new Set(array);
+};
