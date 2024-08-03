c64de65580346edb6b173f3c9d5c76d58af95583
Emmanuel Nnaji <realreaach@yahoo.com>
2024-08-03 05:31:50 +0100

    fxn returns bolean if  all array elem exist in set

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+const hasValuesFromArray = (set, array) => {
+  // Check if the first argument is a Set and the second argument is an Array
+  if (!(set instanceof Set) || !Array.isArray(array)) {
+    throw new Error('Invalid arguments: expected a Set and an Array');
+  }
+
+  // Check if all elements in the array exist in the set
+  return array.every(element => set.has(element));
+};
