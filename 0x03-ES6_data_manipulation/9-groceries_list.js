90e44b9b2aa7b0f23ba6ff330a60675253c6d9be
Emmanuel Nnaji <realreaach@yahoo.com>
2024-08-03 05:43:42 +0100

    returns a map of groceries with the following items (name, quantity)

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default const groceriesList = () => {
+  // Create a new Map with the specified items and quantities
+  const map = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+
+  return map;
+};
